5c032caa4b7c3f8991960e2245100e2ca70913c3
psanchezmor08 <psanchezmor08@gmail.com>
2026-02-13 09:14:38 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 # =====================================================
 # CONFIGURACIÓN INICIAL
 # =====================================================
-st.set_page_config(page_title="Consola de Gestión", page_icon="🖥️", layout="wide")
+st.set_page_config(page_title="Panel de Gestión ADA", page_icon="diseño/logo-icono.png", layout="wide")
 
 # =====================================================
 # GESTIÓN DE CSS GLOBAL
@@ -127,3 +127,4 @@ with col2:
 
 st.write("---")
 st.caption("Conectado al nodo Proxmox: 10.162.130.164")
+
